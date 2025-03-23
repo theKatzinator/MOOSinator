a0ce285fba2bdbfa8fec6a0cc40d84e5479c8b56
Blacky und Flecki Scriptmaster <nc37711@gmail.com>
2025-03-23 12:16:55 +0100

    feat: neue Datei hinzugefügt

--- Beispiel Missionen/moose_test.lua
+++
@@ -1,6 +0,0 @@
--- Testen, ob MOOSE korrekt eingebunden ist
-if not SPAWN then
-    trigger.action.outText("MOOSE wurde nicht richtig geladen!", 10)
-else
-    trigger.action.outText("MOOSE funktioniert!", 10)
-end
